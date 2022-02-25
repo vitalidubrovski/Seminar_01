0dda70e34cd5e3df7abb2362a261e107497b86c4
Noah <vik_026@mail.ru>
2022-02-25 18:31:40 +0300

    massive

---
+++ work3/Program.cs
@@ -0,0 +1,5 @@
+﻿string[] weekDays = new string[] {"Mon", "Tues", "Wed", "Thur", "Frid", "Sat", "Sun"};
+Console.WriteLine("Day number");
+int Daynumber = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(weekDays[Daynumber-1]);
+
