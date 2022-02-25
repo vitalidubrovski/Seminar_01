91d7a0e2245cc9158ac049e150306732d68ac1d4
Noah <vik_026@mail.ru>
2022-02-25 19:53:50 +0300

    task_4

---
+++ Homework4/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите первое число");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите третье число");
+int c = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("А = " + a + " B = " + b + " С = " + c);
+int[] Numbers = {a, b, c};
+int maxValue = Numbers.Max<int>();
+Console.WriteLine("Max = " +maxValue);
