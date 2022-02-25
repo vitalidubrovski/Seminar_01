8ec1f56eeeaa751d55f3ebac6d3263b4f9c9786b
Noah <vik_026@mail.ru>
2022-02-25 20:23:17 +0300

    task_6

---
+++ Homework6/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите число");
+int a = Convert.ToInt32(Console.ReadLine());
+if((a % 2) !=0)
+{
+    Console.WriteLine("Нечетное");
+}
+else
+{
+    Console.WriteLine("Четное");
+}
