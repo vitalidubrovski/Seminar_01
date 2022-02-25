b4ce2955bb2b5d7692251cca904d6c258a3de3fc
Noah <vik_026@mail.ru>
2022-02-25 18:59:50 +0300

    task_2

---
+++ Homework2/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите первое число");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("a = " + a + " b = " +b);
+if(a>b)
+{
+    Console.WriteLine("MAX = " +a + " MIN = " +b);
+}
+else
+{
+    Console.WriteLine("MAX = " +b + " MIN = " +a);
+}
