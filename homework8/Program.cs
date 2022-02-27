679ce1fd57c4329873f7381ccc309f0c5b2ea6a3
Noah <vik_026@mail.ru>
2022-02-27 15:42:08 +0300

    task8

---
+++ homework8/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Введите число");
+int N = Convert.ToInt32(Console.ReadLine());
+int i = -Math.Abs(N);
+for(i = 1; i<=N; i++)
+    if((i % 2)  <=0)
+{
+    Console.Write(""+i+"");
+    i++;
+}
