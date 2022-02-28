238d48d6c7a6e739fc5bcdba7869bd80533daaee
Noah <vik_026@mail.ru>
2022-02-28 20:05:24 +0300

    Process

---
+++ work/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("введите 1 число");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите число 2");
+int number2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Ввели число A " + number1 + " Ввели число B " + number2);
+if(number1*number1 == number2)
+{
+    Console.WriteLine("да");
+}
+else
+{
+    Console.WriteLine("нет");
+}
