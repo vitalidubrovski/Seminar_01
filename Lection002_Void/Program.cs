907e3c1c86238ea5c940587919e7f724ecc1451f
Noah <vik_026@mail.ru>
2022-03-01 20:23:25 +0300

    Voidpractice

---
+++ Lection002_Void/Program.cs
@@ -0,0 +1,51 @@
+﻿void FillArrayNazvanievoida (int [] collection)
+{
+    int length = collection.Length;
+    int index = 0;
+    while(index < length)
+    {
+        collection[index] = new Random().Next(1, 10);
+        index++;
+
+    }
+}
+
+void PrintArrayNazvanieVoida (int [] col)
+{
+    int count = col.Length;
+    int position = 0;
+    while(position < count)
+    {
+        Console.WriteLine(col[position]);
+        position++;
+
+    }
+}
+
+int IndexOff(int [] collection, int find)
+{
+    int count = collection.Length;
+    int index = 0;
+    int position = 0;
+    while(index<count)
+    {
+        if(collection[index] == find)
+        {
+            position = index;
+        }
+        index++;
+    }
+    return position;
+
+}
+
+
+
+int [] array = new int [10];
+
+FillArrayNazvanievoida(array);
+PrintArrayNazvanieVoida(array);
+Console.WriteLine();
+
+int pos = IndexOff(array, 2);
+Console.WriteLine(pos);
