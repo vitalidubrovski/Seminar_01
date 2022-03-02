f0a436ba1e7054dd0a720e158e4fc7b1cbbd589c
Noah <vik_026@mail.ru>
2022-03-02 17:31:21 +0300

    Task10

---
+++ Homework10/Program.cs
@@ -0,0 +1,11 @@
+﻿Random r = new Random();
+int number = r.Next(100, 1000);
+Compare(number);
+
+void Compare(int current_num)
+{
+    int f_place = current_num / 100;
+    int s_place = (current_num / 10) % 10;
+    int t_place = current_num % 10;
+    Console.WriteLine(current_num + ">>>" +t_place+s_place+f_place);
+}
