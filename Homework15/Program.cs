8d7ff5b7d236f8201a0926016cc21a9a6aaddb00
Noah <vik_026@mail.ru>
2022-03-02 18:40:50 +0300

    Task15

---
+++ Homework15/Program.cs
@@ -0,0 +1,35 @@
+﻿Console.WriteLine("День недели");
+int x = Convert.ToInt32(Console.ReadLine());
+WeekEnds(x);
+
+void WeekEnds(int daynumber)
+{
+    if(daynumber == 1)
+    {
+        Console.WriteLine(daynumber + " -> " +  "Нет"  +  " Понедельник ");
+    }
+    if(daynumber == 2)
+    {
+        Console.WriteLine(daynumber + " -> " +  "Нет"  +  " Вторник");
+    }
+    if(daynumber == 3)
+    {
+        Console.WriteLine(daynumber + " -> " +  "Нет"  +  " Среда");
+    }
+    if(daynumber == 4)
+    {
+        Console.WriteLine(daynumber + " -> " +  "Нет"  +  " Четверг");
+    }
+    if(daynumber == 5)
+    {
+        Console.WriteLine(daynumber + " -> " +  "Нет"  +  " Пятница");
+    }
+    if(daynumber == 6)
+    {
+        Console.WriteLine(daynumber + " -> " +  "Да"  +  " Суббота.Выходной");
+    }
+    if(daynumber == 7)
+    {
+        Console.WriteLine(daynumber + " -> " +  "Да"  +  " Воскресенье.Выходной");
+    }
+}
