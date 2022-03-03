612aa9cc3a83dba295f684ca938fbe79d28d3bf3
Noah <vik_026@mail.ru>
2022-03-03 03:05:32 +0300

    Task13

---
+++ Homework13/Program.cs
@@ -0,0 +1,19 @@
+﻿Random r = new Random();
+int number = r.Next(10, 200);
+Compare(number);
+
+
+
+void Compare(int current_num)
+{
+    int a = 100;
+    if(current_num<a)
+    {
+        Console.WriteLine(current_num + " >>> " + " Нет третьей цифры");
+    }
+    else
+    {
+        int t_place = current_num % 10;
+        Console.WriteLine(current_num + " >>> " + t_place);
+    }
+}
