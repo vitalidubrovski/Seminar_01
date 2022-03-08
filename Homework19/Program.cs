9eb8bfb30e459054feaefd1c86cf8bd1f177c567
Noah <vik_026@mail.ru>
2022-03-08 15:43:46 +0300

    Task_19

---
+++ Homework19/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите 5-и значное число");
+string numbers = Convert.ToString(Console.ReadLine());
+if((numbers[0] == numbers[4]) & (numbers[1] == numbers[3]))
+{
+    Console.WriteLine(numbers + " ==> " + " Да ");
+}
+else
+{
+    Console.WriteLine(numbers + " ==> " + " Нет");
+}
