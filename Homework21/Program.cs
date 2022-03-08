e80fa8777be11c8e8cf8af4ff10bee216a115b46
Noah <vik_026@mail.ru>
2022-03-08 13:42:42 +0300

    Task_21

---
+++ Homework21/Program.cs
@@ -0,0 +1,17 @@
+﻿Random rand = new Random();
+
+int a_x = rand.Next(1, 10);
+int a_y = rand.Next(1, 10);
+int a_z = rand.Next(1, 10);
+int b_x = rand.Next(1, 10);
+int b_y = rand.Next(1, 10);
+int b_z = rand.Next(1, 10);
+
+Console.WriteLine("Точка А - " + a_x + ":" + a_y + ":" + a_z);
+Console.WriteLine("Точка В - " + b_x + ":" + b_y + ":" + b_z);
+
+double distance = Math.Sqrt( 
+    (a_x - b_x)*(a_x - b_x) + (a_y - b_y)*(a_y - b_y) +(a_z - b_z)*(a_z - b_z));
+Console.WriteLine("Длина отрезка: " +distance);
+
+//Math.Pow не получилось решить, всегда пишет ошибу связанную с типом данных double(можете разобрать эту задачу на семинаре, с решением через Pow?)
